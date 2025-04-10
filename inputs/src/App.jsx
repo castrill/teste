8534284dbd198ed2c440963ac75cf7431c9831cb
castrill <gabrielcavalheiro2005@gmail.com>
2025-04-10 20:15:49 -0300

    Calculadeira feita em React funcional

--- inputs/src/App.jsx
+++ inputs/src/App.jsx
@@ -6,24 +6,43 @@ function App(){
 
   const[inputN1, setInputN1] = useState('')
   const[inputN2, setInputN2] = useState('')
+  const[resultado, setResultado] = useState('')
+
+  function somar() {
+    setResultado(Number(inputN1) + Number(inputN2))
+  }
+  function subtrair() {
+    setResultado(Number(inputN1) - Number(inputN2))
+  }
+  function dividir() {
+    setResultado(Number(inputN1) / Number(inputN2))
+  }
+  function multiplicar() {
+    setResultado(Number(inputN1) * Number(inputN2))
+  }
 
   function tratarN2(event) {
     setInputN2(event.target.value)
 
   }
+
+
   return(
     <>
     <h1>Calculadeira</h1>
-    <div>
-      <input type="text" value={inputN1} onChange={(event) => setInputN1(event.target.value)} />
-      {inputN1}
-      <input type="text" value={inputN2} onchange={tratarN2} />
-      {inputN2}
-
-
-    </div>
+    <div className="container-app">
+      <input type="number" step="1" value={inputN1} onChange={(event) => setInputN1(event.target.value)} />
+    😶‍🌫️
+      <input type="text" value={inputN2} onChange={(event) => setInputN2(event.target.value)}/>      
+      </div>
+  
+    <button onClick={somar}>+</button>
+    <button onClick={subtrair}>-</button>
+    <button onClick={dividir}>/</button>
+    <button onClick={multiplicar}>*</button>
+    = {resultado}
     </>
-  )
-}
+    )
+  }
 
 export default App
